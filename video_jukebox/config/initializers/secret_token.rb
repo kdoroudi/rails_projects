641e98f873589357019264dfca005eb88f3c7917
Kaveh Doroudi <smart4ce@gmail.com>
2012-06-26 00:44:20 -0400

    project one

---
+++ video_jukebox/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoJukebox::Application.config.secret_token = '90e3ad48dac338d1b2893869a5a51c921310a89823472b459564a5f3b38d7ae9fb0d3b9dc93cd6102b3e0eaea07afd9df632df886a8e5f4f0620845950a76907'
